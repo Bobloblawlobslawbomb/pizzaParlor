6af91943b87f54ad964245655c32eeab38fde5ae
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-09 13:17:58 -0700

    WIP: update UI logic adding forms and buttons

--- js/scripts.js
+++ js/scripts.js
@@ -18,15 +18,22 @@ $(document).ready(function () {
   $("#NAMEOFOFORM").submit(function (e) {
     e.preventDefault();
     const personInput = $('input#userNum').val()
-    $("#result").html(beepBoop(personInput))
+    $("#result").html(beepBoop(personInput))//see next comment for sample WIP
   })
 })
+/*
+//const personPizza = $('input#userNum').val()
+$("#result").html(personPizza.price())
+*/
 
 //USER INTERFACE LOGIC
 $(document).ready(function () {
-  $("#NAMEOFOFORM").submit(function (e) {
+  let userPizzaToppings
+  let userPizzaSize
+  $("#form-one").submit(function (e) {
     e.preventDefault();
-    const personPizza = $('input#userNum').val()
-    $("#result").html(personPizza.price())
+    userPizzaToppings = $("#radio-one-div input[type='radio']:checked");
+    $("#form-two").slideDown();
+    $("#form-one").slideUp();
   })
 })
