9d77c7d26f5c764a783f29954c07bff1735e56ec
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-11 23:59:03 -0700

    WIP checkboxes --only pepperoni pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -15,15 +15,18 @@ $(document).ready(function () {
   let userPizzaSize
   $("#form-one").submit(function (e) {
     e.preventDefault();
-    userPizzaToppings = $("#radio-one-div input[type='radio']:checked").val();
+    userPizzaToppings = $("#radio-one-div input[type='checkbox']:checked").val();
+    console.log("This is the userPizzaToppings: " + userPizzaToppings)//debug
     $("#form-two").slideDown();
     $("#form-one").slideUp();
   })
   $("#form-two").submit(function (e) {
     e.preventDefault();
     userPizzaSize = $("#radio-two-div input[type='radio']:checked").val();
+    console.log("This is the userPizzaSize: " + userPizzaSize)//debug
     $("#results").slideDown();
     const userPizzaOne = new Pizza(userPizzaToppings, userPizzaSize);
+    console.log("This is userPizzaOne: " + userPizzaOne)//debug
     $("#price-place").html("$" + userPizzaOne.price() + ".99");
     $("#form-two").slideUp();
   })
